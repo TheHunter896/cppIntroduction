87f81e40850f8b203b3a57eff12c6bd21612fef8
--replace <david_corp896@outlook.es>
2020-05-27 17:58:52 +0200

    Update class_constructor.cpp

--- section_oop/class_constructor.cpp
+++ section_oop/class_constructor.cpp
@@ -95,3 +95,68 @@ Player::Player(std::string, int health, int xp, int level) //Constructor with in
 Player::Player(std::string name="None", int health=0, int xp=0, int level=0) //This will be used even with empty, take care of default values, you obv dont want to set defaults ALWAYS, depends on case.
     : name{name}, health{health}, xp{xp}, level{level}{ //Initialization list
 }
+
+//Copy constructs between classes
+
+//Imagine this case
+Player empty {"Null", 100, 50}; //Define an empty class, or any class for that matter
+
+Player my_new_obj {empty}; //In this case the constructor has to be something that initializes with a class, not with parameters
+
+Player::Player(const Player &source) //This constructor defines the class init. It's not direct inheritance but initializing.
+    : name(source.name), health(source.health), xp(source.xp), level(source.level){
+        std::cout << "Copy constructor";
+}
+
+//Copying and shallow copying constructors.
+/*
+ * Imagine you have obj1 that has a pointer to 98
+ * Now you copy that object by passing a parameter
+ * The copy will execute the constructor which will call our copy constructor
+ * If that copy constructor does not make a deep copy then both obj1 and its copy will point to the same space
+ * When the copy goes out of scope the destructor will destroy the pointer, and release the storage
+ * Making obj1 crash when it accesses that reference again.
+ */
+
+/*Practical example in why this is important */
+
+class Deep{
+private:
+    int *data;
+public:
+    void set_data_value(int d) {*data = d;}
+    int get_data_value(){return *data;}
+    //Constructor
+    Deep(int d);
+    //Copy constructor
+    Deep(const Deep &source);
+    //Destructor
+    ~Deep();
+};
+
+Deep::Deep(int d) {
+    data = new int;
+    *data = d;
+}
+
+Deep::Deep(const Deep &source)
+    : Deep{*source.data}{
+
+}
+
+Deep::~Deep(){
+    delete data;
+}
+
+void display_deep(Deep s){
+    std::cout << s.get_data_value() << std::endl;
+}
+
+int deep(){
+    Deep obj1 {100};
+    display_deep(obj1); //Here obj1 will make a copy of itself -> call copy constructor -> call default constructor -> call get_data_value (s), s being a copy of obj1
+    //When going out of display_deep if the pointer of s is the same as obj1 the value will be lost!
+    //Since its a copy AND we have declared Deep::Deep(const Deep &source) the reference is different
+    //When going out the destructor of Deep will be called on obj1 and the reference will be deleted, but it will not affect obj1, only s
+    return 0;
+}
