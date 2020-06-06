db7dab5821314aca61d295da4706757daa7eb35c
--replace <david_corp896@outlook.es>
2020-06-06 12:01:50 +0200

    Update polymorphism.cpp

--- section_polymorphism/polymorphism.cpp
+++ section_polymorphism/polymorphism.cpp
@@ -11,19 +11,34 @@ using namespace std;
 
 class Base{
 public:
-    void say_hello(){
+
+    virtual void say_hello() const{
         cout << "Hi i am Base" << endl;
     }
+    virtual ~Base(){
+        cout << "Account destructor" << endl;
+    };
 };
 
 class Derived: public Base{
 public:
-    void say_hello(){
-        cout << "Hi i am Derived"; endl;
+
+    //This is polymorphism at it's peak, it means abstraction and generalization
+    virtual void say_hello() const override { //override the original one, be careful because if the original one is const, you should make const override
+        cout << "Hi i am Derived" << endl;
+    }
+
+    virtual ~Derived(){
+        cout << "Derived destructor";
     }
 };
 
+//Be careful when overriding/overwriting, overwriting does not re bind the object.
+//In case you use pointers you should point to the base class and add an override operator to say hello
 
+class Shape {
+    []
+};
 
 int polly(){
 
@@ -33,9 +48,12 @@ int polly(){
     Derived d;
     d.say_hello(); //this is bound to d, which is derived
 
-    Base *ptr = new Derived();
-    ptr->say_hello(); //PTR can point to any Base object, derived IS a based object. Compiler only knows that ptr points to a base object.
+    Base *ptr = new Derived(); //-> say_hello bound to derived now, due to the const override
+    ptr->say_hello(); //PTR can point to any Base object, derived IS a base object. Compiler only knows that ptr points to a base object.
     //say_hello will be bound to say_hello
+
+    //Now if we make the methods virtual the thing changes. Vritual means the methods will be overwritten when inherited (if a bound method exists)
+    //You can also define virtual destructors
 
     return 0;
 }
