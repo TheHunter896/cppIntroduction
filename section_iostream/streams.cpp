e8ffb898e0a6f72a08e3e79a42ccd9def50d6f22
--replace <david_corp896@outlook.es>
2020-06-19 08:54:24 +0200

    Added file manipulation (reading/writing)

--- section_iostream/streams.cpp
+++ section_iostream/streams.cpp
@@ -59,6 +59,48 @@ int string_formatting(){
     //These can be used with setf and resetiosflags
 
     //Floats
+    float fl {3322.32123};
+    std::cout << fl << std::endl; //Standard is 6 values from most relevant int
+    //set the precision
+    std::cout << std::setprecision(3)
+              << std::scientific
+              << std::uppercase;
+
+    std::cout << fl << std::endl;
+    //Statements above display fl with precision 3, +E+03 (notice the E is uppercase)
+
+    //Reading and writing to text files
+#include <fstream>
+
+    //Fstream and ifstream are the main used objects
+    std::fstream in_file {"../myfile.txt", std::ios::in}; //Up from the executable lives, open reading as bytes
+    //Std::ios::in means input stream mode, its also the default option
+
+    std::fstream in_file {"../myfile.txt", std::ios::in | std::ios::binary}; //Open reading as binary
+
+    std::ifstream in_file;
+    std::string filename;
+    std::cin >> filename;
+
+    in_file.open(filename);
+    //or
+    in_file.open(filename, std::ios::binary);
+
+    //Checking if its open
+    if(in_file.is_open()){
+        std::cout << "File was opened successful" << std::endl;
+    }
+    in_file.close(); //Close the thingie after
+
+    //Writing
+    std::ofstream out_file {"../my_file.txt", std::ios::out}; //Std is optional
+
+    //Different openings
+    std::ofstream out_file {"../my_file.txt", std::ios::trunc}; //Std is optional
+
+    std::ofstream out_file {"../my_file.txt", std::ios::app}; //Std is optional
+
+    std::ofstream out_file {"../my_file.txt", std::ios::ate}; //Std is optional
 
 
     return 0;
